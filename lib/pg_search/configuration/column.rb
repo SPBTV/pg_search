c52157c969ce2e63d88bc2cc4689918b17e071fc
Ben Hadley-Evans <ben@ebip.co.uk>
2015-05-18 17:19:43 +0100

    Avoid casting a tsvector column to text and back again.
    When you have a tsvector column but want to search against both it and other text columns (without having to deal with triggers) the :tsvector_column option is no use as it causes everything in :against to be ignored. Also since a column's type is available I don't see why the need to specify something as a tsvector column. This change allows you to simply pass column names to :against, and if any of those are tsvectors then they will be used directly, otherwise they will be converted to tsvectors as before.
    
    My data set is over 900 records and as well as the document I search 4 additional small text fields, also there is an postgres gin index being used. On a query that matched just 4 results the speed improved from 40ms to 3ms. And on a query that matched 397 results the speed improved from over 3000ms to 60ms.

--- lib/pg_search/configuration/column.rb
+++ lib/pg_search/configuration/column.rb
@@ -13,15 +13,28 @@ module PgSearch
         @weight = weight
       end
 
-      def to_sql
-        "coalesce(#{@column.to_sql(connection, *to_sql_options)}::text, '')"
-      end
-
       def full_name
         @column.full_name(connection, *full_name_options)
       end
 
+      def to_sql
+        coalesce("#{expression}::text")
+      end
+
+      def to_sql_no_cast
+        coalesce(expression)
+      end
+
+      def tsvector?
+        psql_column = @model.columns_hash[name]
+        psql_column && psql_column.type.eql?(:tsvector)
+      end
+
       private
+
+      def expression
+        @column.to_sql(connection, *to_sql_options)
+      end
 
       def connection
         @model.connection
@@ -33,6 +46,10 @@ module PgSearch
 
       def full_name_options
         [@model.table_name]
+      end
+
+      def coalesce(value)
+        "coalesce(#{value}, '')"
       end
     end
   end
